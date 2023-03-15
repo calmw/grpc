bca575b5a3bfbf691cc60a5d0fa686e92ccfa75d
cisco <codingmaster.wang@outlook.com>
2023-03-15 10:55:39 +0800

    服务端紧急处理（panic）拦截器 防止服务端Panic后终止服务 服务端一元超时处理拦截器

--- client/main.go
+++ client/main.go
@@ -1,80 +1,207 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	svc "github.com/calmw/grpc-service"
 	"google.golang.org/grpc"
-	healthz "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("Specify a gRPC server")
+	if len(os.Args) != 3 {
+		log.Fatal("Specify a gRPC server and method to call")
 	}
 	serverAddr := os.Args[1]
+	methodName := os.Args[2]
 
-	healthClient, err := getHealthSvcClient(serverAddr)
+	// 获取TLS证书
+	tlsCertFile, ok := os.LookupEnv("TLS_CERT_FILE")
+	if !ok {
+		tlsCertFile = "./server.crt"
+	}
+	conn, err := setupGrpcConnection(serverAddr, tlsCertFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 测试 check
-	resp, err := healthClient.Check(
-		context.Background(),
-		&healthz.HealthCheckRequest{
-			Service: "Users", // 如果指定一个没有设置健康状态的服务，将得到一个非nil错误的响应，并且响应的resp为nil，错误响应码将设置为codes.NotFound
-		},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	healthStatus := resp.Status.String()
-	if healthStatus != "SERVING" {
-		log.Fatalf(
-			"Expected status SERVING, got %s",
-			healthStatus,
+	defer conn.Close()
+	c := getUserServiceClient(conn)
+	switch methodName {
+	case "GetUser":
+
+		result, err := c.GetUser(context.Background(), &svc.UserGetRequest{
+			Email: "panic@doe.com", // @前为panic时触发服务端panic，其它正常
+		})
+
+		s := status.Convert(err) // status.Convert函数分别访问错误代码和错误消息
+		if s.Code() != codes.OK {
+			log.Fatalf("Request failed: %v-%v\n", s.Code(), s.Message())
+		}
+		fmt.Fprintf(
+			os.Stdout,
+			"User: %s %s\n",
+			result.User.FirstName,
+			result.User.LastName,
 		)
-	}
-	log.Println("Check, SERVING...")
 
-	// 测试watch
-	client, err := healthClient.Watch(
-		context.Background(),
-		&healthz.HealthCheckRequest{
-			Service: "Users", // 如果指定一个没有设置健康状态的服务，将得到一个非nil错误的响应，并且响应的resp为nil，错误响应码将设置为codes.NotFound
-		},
-	)
+	case "GetHelp":
+		err = setupChat(os.Stdin, os.Stdout, c)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Unrecognized method name")
+	}
+
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	for {
-		resp, err = client.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Error in watch: %v \n", err)
-		}
-		log.Printf("Health status: %#v \n", resp)
-		if resp.Status != healthz.HealthCheckResponse_SERVING {
-			log.Printf("Unhealthy: %#v \n", resp)
-		}
 	}
 
 }
 
-func getHealthSvcClient(addr string) (healthz.HealthClient, error) {
-
-	client, err := grpc.DialContext(
-		context.Background(),
-		addr,
-		grpc.WithBlock(),    // 确保在函数返回之前建立连接。这意味着如果在服务器启动并运行之前运行客户端，它将无限期等待。
-		grpc.WithInsecure(), // 与服务器建立非TSL连接
+// 一元客户端拦截器，对传出的任何一元RPC请求添加一个唯一标识符，添加的数据在context中
+func metadataUnaryInterceptor(
+	ctx context.Context,
+	method string,
+	req, reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption,
+) error {
+	ctxWithMetadata := metadata.AppendToOutgoingContext(
+		ctx,
+		"Request-Id",
+		"request-123",
 	)
+	return invoker(
+		ctxWithMetadata,
+		method,
+		req,
+		reply,
+		cc,
+		opts...,
+	)
+}
+
+// 流RPC方法调用的元数据拦截器
+func metadataStreamInterceptor(
+	ctx context.Context,
+	desc *grpc.StreamDesc,
+	cc *grpc.ClientConn,
+	method string,
+	streamer grpc.Streamer,
+	opts ...grpc.CallOption,
+) (grpc.ClientStream, error) {
+
+	ctxWithMetadata := metadata.AppendToOutgoingContext(
+		ctx,
+		"Request-Id",
+		"request-123",
+	)
+	stream, err := streamer(
+		ctxWithMetadata,
+		desc,
+		cc,
+		method,
+		opts...,
+	)
+	clientStream := wrappedClientStream{ClientStream: stream} // 对数据进行包装
+
+	return clientStream, err
+}
+
+func setupGrpcConnection(addr, tlsCertFile string) (*grpc.ClientConn, error) {
+	creds, err := credentials.NewClientTLSFromFile(tlsCertFile, "") // 如果第二个参数非空，将覆盖在证书中找到的主机名。并且该主机名将被信任。我们将为localhost主机名生成TLS证书，这是我们希望客户端信任的主机名，因此指定一个空字符串。
 	if err != nil {
 		return nil, err
 	}
-	return healthz.NewHealthClient(client), nil
+
+	credsOption := grpc.WithTransportCredentials(creds)
+
+	return grpc.DialContext(
+		context.Background(),
+		addr,
+		credsOption,
+		grpc.WithBlock(), // 确保在函数返回之前建立连接。这意味着如果在服务器启动并运行之前运行客户端，它将无限期等待。
+		grpc.WithChainUnaryInterceptor( // 用于注册多个客户端一元拦截器，最内层的拦截器首先执行
+			metadataUnaryInterceptor,
+			// ... 其他拦截器
+		),
+		grpc.WithChainStreamInterceptor( // 用于注册多个客户端流拦截器，最内层的拦截器首先执行
+			metadataStreamInterceptor,
+			// ... 其他拦截器
+		),
+	)
+}
+
+func getUserServiceClient(conn *grpc.ClientConn) svc.UsersClient {
+	return svc.NewUsersClient(conn)
+}
+
+func setupChat(r io.Reader, w io.Writer, c svc.UsersClient) error {
+
+	stream, err := c.GetHelp(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for {
+		scanner := bufio.NewScanner(r)
+		prompt := "Request: "
+		fmt.Fprint(w, prompt)
+
+		scanner.Scan()
+		if err = scanner.Err(); err != nil {
+			return err
+		}
+		msg := scanner.Text()
+		if msg == "quit" {
+			break
+		}
+		request := svc.UserHelpRequest{
+			Request: msg,
+		}
+
+		err = stream.Send(&request)
+		if err != nil {
+			return err
+		}
+
+		resp, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Response: %s\n", resp.Response)
+
+	}
+
+	return stream.CloseSend() // 该方法将关闭服务器上的客户端链接，并返回io.EOF错误值
+}
+
+// 下面的一个结构体以及方法，是对客户端流的包装，将使用这些方法对原本流处理方法进行替换，来对客户端流的包装，实现每次流传输都可以进行自定义操作，而不是原本的等到全部传输完成才执行拦截器
+type wrappedClientStream struct {
+	grpc.ClientStream
+}
+
+func (s wrappedClientStream) SendMsg(m interface{}) error {
+	log.Printf("Send msg called: %T", m)
+	return s.ClientStream.SendMsg(m)
+}
+
+func (s wrappedClientStream) RecvMsg(m interface{}) error {
+	log.Printf("Recv msg called: %T", m)
+	return s.ClientStream.RecvMsg(m)
+}
+
+func (s wrappedClientStream) CloseSend() error {
+	log.Printf("CloseSend() called")
+	return s.ClientStream.CloseSend()
 }
--- server/server.go
+++ server/server.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	svc "github.com/calmw/grpc-service"
 	"google.golang.org/grpc"
-	healthsvc "google.golang.org/grpc/health"
-	healthz "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -20,11 +27,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// 获取TLS密钥和证书
+	tlsCertFile, ok := os.LookupEnv("TLS_CERT_FILE")
+	if !ok {
+		tlsCertFile = "./server.crt"
+	}
+	tlsKeyFile, ok := os.LookupEnv("TLS_KEY_FILE")
+	if !ok {
+		tlsKeyFile = "./server.key"
+	}
 
-	s := grpc.NewServer()
-	h := healthsvc.NewServer()
-	registerServices(s, h)
-	updateServiceHealth(h, svc.Users_ServiceDesc.ServiceName, healthz.HealthCheckResponse_SERVING)
+	creds, err := credentials.NewServerTLSFromFile(
+		tlsCertFile,
+		tlsKeyFile,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	credsOption := grpc.Creds(creds)
+	s := grpc.NewServer(
+		credsOption,
+		grpc.ChainUnaryInterceptor( // 用于注册多个服务端一元拦截器，最内层的拦截器首先执行
+			loggingUnaryInterceptor,
+			panicUnaryInterceptor,
+			// ... 其他拦截器
+		),
+		grpc.ChainStreamInterceptor( // 用于注册多个服务端流拦截器，最内层的拦截器首先执行
+			loggingStreamInterceptor,
+			panicStreamInterceptor,
+			// ... 其他拦截器
+		),
+	)
+	registerServer(s)
 	log.Fatal(startServer(s, lis))
 }
 
@@ -32,9 +66,8 @@ type userService struct {
 	svc.UnimplementedUsersServer // 对于grpc中任何服务实现都是强制性的
 }
 
-func registerServices(s *grpc.Server, h *healthsvc.Server) {
+func registerServer(s *grpc.Server) {
 	svc.RegisterUsersServer(s, &userService{})
-	healthz.RegisterHealthServer(s, h)
 	reflection.Register(s)
 }
 
@@ -42,10 +75,201 @@ func startServer(s *grpc.Server, l net.Listener) error {
 	return s.Serve(l)
 }
 
-func updateServiceHealth(
-	h *healthsvc.Server,
-	service string,
-	status healthz.HealthCheckResponse_ServingStatus,
+func (s *userService) GetHelp(stream svc.Users_GetHelpServer) error {
+	log.Println("Client connected")
+
+	for {
+		request, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Request received: %s \n", request.Request)
+
+		if request.Request == "panic" {
+			panic("I was asked to panic")
+		}
+
+		response := svc.UserHelpReply{Response: request.Request}
+
+		err = stream.Send(&response)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Println("Client disconnected")
+	return nil
+}
+
+func (s *userService) GetUser(ctx context.Context, in *svc.UserGetRequest) (*svc.UserGetReply, error) {
+	log.Printf(
+		"Received request for user with Email: %s Id:%s\n",
+		in.Email,
+		in.Id,
+	)
+	components := strings.Split(in.Email, "@")
+	if len(components) != 2 {
+		return nil, status.Error(codes.InvalidArgument, "Invalid email address specified") // status.Error函数创建错误，可以错误码和错误信息一块创建
+	}
+	if components[0] == "panic" {
+		panic("I was asked to panic")
+	}
+	u := svc.User{
+		Id:        in.Id,
+		FirstName: components[0],
+		LastName:  components[1],
+		Age:       36,
+	}
+	return &svc.UserGetReply{User: &u}, nil
+}
+
+// 服务端，一元RPC方法调用的日志拦截器
+func loggingUnaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	start := time.Now()
+	resp, err := handler(ctx, req)
+	logMessage(ctx, info.FullMethod, time.Since(start), err)
+
+	return resp, err
+}
+
+// 服务端，一元紧急处理拦截器
+func panicUnaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (resp interface{}, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Panic recovered: %v", r)
+			err = status.Error(
+				codes.Internal,
+				"Unexpected error happened",
+			)
+		}
+	}()
+	resp, err = handler(ctx, req)
+
+	return
+}
+
+// 服务端，一元超时终止请求拦截器
+// 假设我们想对RPC方法执行时间施加一个上限，我们知道对于某些恶意请求用户，RPC调用方法可能需要比300毫秒更长的时间，这种情况下我们只想终止请求
+// 任何超过300毫秒的RPC方法都将被终止
+func timeoutUnaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	var resp interface{}
+	var err error
+
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel()
+
+	ch := make(chan error)
+	go func() {
+		resp, err = handler(ctxWithTimeout, req)
+		ch <- err
+	}()
+
+	select {
+	case <-ctxWithTimeout.Done():
+		cancel()
+		err = status.Error(
+			codes.DeadlineExceeded,
+			fmt.Sprintf("%s: DeadlineExceeded", info.FullMethod),
+		)
+		return resp, err
+	case <-ch:
+	}
+
+	return resp, err
+}
+
+// 服务端，流RPC调用的日志拦截器
+func loggingStreamInterceptor(
+	srv interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) error {
+	start := time.Now()
+	serverStream := wrappedServerStream{ServerStream: stream}
+	err := handler(srv, serverStream)
+	ctx := stream.Context()
+	logMessage(ctx, info.FullMethod, time.Since(start), err)
+
+	return err
+}
+
+// 服务端，流紧急处理拦截器
+func panicStreamInterceptor(
+	srv interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Panic recovered: %v", r)
+			err = status.Error(
+				codes.Internal,
+				"Unexpected error happened",
+			)
+		}
+	}()
+	serverStream := wrappedServerStream{ServerStream: stream}
+	err = handler(srv, serverStream)
+
+	return
+}
+
+//记录RPC方法调用的详细信息
+func logMessage(
+	ctx context.Context,
+	method string,
+	latency time.Duration,
+	err error,
 ) {
-	h.SetServingStatus(service, status)
+	var requestId string
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		log.Println("No metadata in context")
+	} else {
+		if len(md.Get("Request-Id")) != 0 {
+			requestId = md.Get("Request-Id")[0]
+		}
+	}
+	log.Printf(
+		"Method: %s, Latency: %v, Error: %v, RequestId: %s",
+		method,
+		latency,
+		err,
+		requestId,
+	)
+}
+
+// 下面的一个结构体以及方法，是对服务端流的包装，将使用这些方法对原本流处理方法进行替换，来对服务端流的包装，实现每次流传输都可以进行自定义操作，而不是原本的等到全部传输完成才执行拦截器
+type wrappedServerStream struct {
+	grpc.ServerStream
+}
+
+func (s wrappedServerStream) SendMsg(m interface{}) error {
+	log.Printf("Send msg called: %T", m)
+	return s.ServerStream.SendMsg(m)
+}
+
+func (s wrappedServerStream) RecvMsg(m interface{}) error {
+	log.Printf("Waiting to receive a msg: %T", m)
+	return s.ServerStream.RecvMsg(m)
 }
